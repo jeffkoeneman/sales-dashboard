150a8936552227d278f482862eea285d42ce08ee
Jeff Koeneman <jeff.koeneman@tifin.com>
2026-04-10 16:19:19 -0600

    update from claude

--- push.ps1
+++ push.ps1
@@ -1,3 +1,4 @@
+Copy-Item -Path "$env:USERPROFILE\Downloads\index.html" -Destination "public\index.html" -Force
 git add .
-git commit -m 'update from claude'
+git commit -m "update from claude"
 git push
